d0d41e40054c6dc9798a0806e928fbf792be16e5
Rafath-Auvee <rbza98@outlook.com>
2023-04-07 10:05:33 +0600

    Signup dispatch working

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "./../../firebase/firebase.config";
+
 const initialState = {
   email: "",
   role: "",
@@ -7,9 +10,37 @@ const initialState = {
   error: "",
 };
 
+export const createUser = createAsyncThunk(
+  "auth/createUser",
+  async ({ email, password }, thunkAPI) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.error = "";
+        state.email = payload;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message;
+        state.email = "";
+      });
+  },
 });
 
 export default authSlice.reducer;
