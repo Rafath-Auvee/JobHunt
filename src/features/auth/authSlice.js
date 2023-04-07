db6b7640eca4c4f0efe245a70f009e291c2a911b
Rafath-Auvee <rbza98@outlook.com>
2023-04-07 10:29:03 +0600

    work

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -10,7 +10,13 @@ const initialState = {
   error: "",
 };
 
-
+export const createUser = createAsyncThunk(
+  "auth/createUser",
+  async ({ email, password }, thunkAPI) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
