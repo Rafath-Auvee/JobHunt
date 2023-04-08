83e83b0b7ad816335c5de74da94ae28cc5e77206
Rafath-Auvee <rbza98@outlook.com>
2023-04-08 09:16:54 +0600

    Employee user data submitted properly

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./../features/auth/authSlice";
+import apiSlice from "./../features/api/apiSlice";
 
 export const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
